7577652e9f311ec9cff68b8418e8898ba6d96d55
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-01-20 17:06:35 +0000

    split out cardSelection function from event listener function and add card as parameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,83 @@
+let animalsArray = ['Cat', 'Dog', 'Mouse', 'Monkey', 'Sheep', 'Cow', 'Pig', 'Horse'];
+let colorArray = ['Blue', 'Green', 'Purple', 'Orange', 'Yellow'];
+let correctAnswers = 0;
+
+
+
+
+
+
 // Wait for the DOM to finish loading before running the game
 // Get the buttom elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName('button');
 
     for (let button of buttons) {
-        button.addEventListener('click', function(){
+        button.addEventListener('click', function () {
             alert("you pressed a button")
-        }) 
+        })
     };
 
     runGame();
 
     let cards = document.getElementsByClassName('card');
-    
+
     for (let card of cards) {
-        card.addEventListener('click', function() {
-            if (animalsArray.includes(card.textContent)) {
-                card.classList.add('correct-card');
-                correctAnswers += 1;
-                if (correctAnswers == 3) {
-                    alert('Congratulations you have won!');
-                }
-            } else {
-                alert('Incorrect, sorry! ' + card.textContent + ' is not an animal');
-            }
-        }) 
+        card.addEventListener('click', function () {
+            cardSelection(card);
+        })
     };
 })
 
+function cardSelection(card) {
 
-let animalsArray = ['Cat', 'Dog', 'Mouse', 'Monkey', 'Sheep', 'Cow', 'Pig', 'Horse'];
-let colorArray = ['Blue', 'Green', 'Purple', 'Orange', 'Yellow'];
-let correctAnswers = 0; 
+    if (animalsArray.includes(card.textContent)) {
+
+        card.classList.add('correct-card');
+        correctAnswers += 1;
+
+        if (correctAnswers == 3) {
+            alert('Congratulations you have won!');
+        }
+    } else {
+        alert('Incorrect, sorry! ' + card.textContent + ' is not an animal');
+    }
+}
+
 
 
 
 
 // For each element in animalsArray, creat a div with the name of the animal and add it to the DOM
-function runGame(){
+function runGame() {
     let animalsArray2 = [...animalsArray]
     let newArray = []
 
     for (let i = 0; i < 3; i++) {
         // Loop through animalsArray and get three random animals and push into newArray
-        let animalIndex = Math.floor(Math.random()*animalsArray2.length);
+        let animalIndex = Math.floor(Math.random() * animalsArray2.length);
         newArray.push(animalsArray2[animalIndex]);
         // Remove animals pushed into newArray from the animalsArray2 so they cannot be selected more than once
         animalsArray2.splice(animalIndex, 1);
     }
 
     for (let i = 0; i < 2; i++) {
-        let colors = Math.floor(Math.random()*colorArray.length);
+        let colors = Math.floor(Math.random() * colorArray.length);
         newArray.push(colorArray[colors]);
         colorArray.splice(colors, 1)
     }
-    
-    console.log(newArray);
-    
 
-    let card = '';
+    console.log(newArray);
+
+
+    let cardHtml = '';
     for (let i of newArray) {
-        card += `<div class="card">${i}</div>`; 
+        cardHtml += `<div class="card">${i}</div>`;
     }
 
     let cardArea = document.getElementById('card-area');
-    cardArea.innerHTML = card;
+    cardArea.innerHTML = cardHtml;
 }
 
 

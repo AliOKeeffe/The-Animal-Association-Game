795392a1cb9595718a047d21446962ebba4329f9
Alison O'Keeffe <okeeffe.ali@gmail.com>
2022-01-27 12:05:32 +0000

    Add timer, counter, shuffle cards and game reset functions to new code and get home buttons linked to index.html

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,5 +1,9 @@
 let gameArea = document.getElementById('game-area');
 let welcomeArea = document.getElementById('welcome-area');
+let gameOver = document.getElementById('gameover');
+let correctAnswers = 0;
+let incorrectAttempts = 0;
+let counter = document.getElementById('counter');
 
 let gameContents = {
     farm: {
@@ -9,6 +13,10 @@ let gameContents = {
     sea: {
         rightAnimals: ['Whale', 'Dolphin', 'Octopus', 'Turtle', 'Seahorse', 'Jellyfish', 'Crab'],
         wrongAnimals: ['Sheep', 'Cow', 'Pig', 'Horse', 'Sloth', 'Monkey', 'Snake'],
+    },
+    jungle: {
+        rightAnimals: ['Sloth', 'Monkey', 'Snake', 'Treefrog', 'Toucan', 'Parrot', 'Leopard'],
+        wrongAnimals: ['Whale', 'Dolphin', 'Octopus', 'Horse','Chicken', 'Cockeral', 'Goat'],
     }
 }
 
@@ -61,7 +69,19 @@ function buildAnimalArray(rightAnimals, wrongAnimals) {
         wrongAnimals.splice(wrongIndex, 1)
     }
 
+    shuffleArray(gameAnimals);
+    
     return gameAnimals;    
+}
+
+// Shuffle order of cards in gameAnimals array using the Fischer Yates Shuffle - https://javascript.info/task/shuffle 
+
+function shuffleArray(gameAnimals) {
+    for (let i = gameAnimals.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+
+        [gameAnimals[i], gameAnimals[j]] = [gameAnimals[j], gameAnimals[i]];
+    }
 }
 
 function writeCards(gameAnimals) {
@@ -80,6 +100,86 @@ function writeCards(gameAnimals) {
 }
 
 function selectCard() {
-    console.log(currentRightAnimals.includes(this.textContent) ? "correct" : "incorrect");
+    if (currentRightAnimals.includes(this.textContent)) {
+
+        this.classList.add('correct-card');
+        correctAnswers += 1;
+        this.removeEventListener('click', selectCard);
+
+        if (correctAnswers == 3) {
+            winGame();
+        }
+    } else {
+        alert('Incorrect, sorry! ' + this.textContent + ' is not a sea animal');
+        incorrectAttemptsCounter()
+    }
 }
 
+function winGame () {
+    let finalCount = document.getElementById('final-count');
+    let finalTime = document.getElementById('final-time');
+
+    gameOver.classList.remove('hide');
+    finalCount.innerHTML = incorrectAttempts;
+    finalTime.innerHTML = `${minutes} minutes and ${seconds} seconds`;
+
+    resetGame();
+}
+
+function incorrectAttemptsCounter() {
+    incorrectAttempts++;
+    counter.innerHTML = incorrectAttempts;
+}
+
+// start timer - credit to https://dev.to/shantanu_jana/create-a-simple-stopwatch-using-javascript-3eoo
+
+let milliseconds = 00;
+let seconds = 00; 
+let minutes = 00;
+let timer = document.getElementById('timer');
+
+setInterval(function() {
+    startCount();
+}, 10);
+ 
+function startCount () {
+    milliseconds +=10;
+    if (milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if (seconds == 60) {
+        seconds = 00;
+        minutes++;
+    }
+    if (minutes == 30) {
+        minutes = 0;
+    }
+
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 100 ? "0" + milliseconds : milliseconds;
+
+    timer.innerHTML = `Timer: ${m} : ${s} : ${ms}`;
+}
+
+function resetGame() {
+    
+    let cardArea = document.getElementById('card-area');
+    cardArea.innerHTML = '';
+  
+    // Reset Score
+    correctAnswers = 0 
+    incorrectAttempts = 0
+    counter.innerHTML = incorrectAttempts;
+    
+    // Reset Timer
+    milliseconds = 00;
+    seconds = 00;
+    minutes = 00;
+    zeroPlaceholder = 0;
+
+    //Reset global variables
+    currentRightAnimals = [];
+    currentWrongAnimals = [];
+}
